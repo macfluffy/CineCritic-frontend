00ceb3a04619645047400cdbf887f182c00ca8a2
macfluffy <shadowimitation@hotmail.com>
2026-04-19 18:59:54 +1000

    feat: hide the allowed hosts into the env file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    allowedHosts: ["cinecritic-load-balancer-351804049.ap-southeast-2.elb.amazonaws.com"],
-    port: 5173,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      allowedHosts: env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',') : [],
+      port: 5173,
     watch: {
       usePolling: true,
     },
