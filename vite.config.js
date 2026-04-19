8f5d539249076878aac99508383c3e83e1d3b4fa
macfluffy <shadowimitation@hotmail.com>
2026-04-19 19:16:14 +1000

    feat: Hardcoded the allowed host again. Temporary fix

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    server: {
-      host: true,
-      allowedHosts: env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',') : [],
-      port: 5173,
-      watch: {
-        usePolling: true,
-      },
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    // allowedHosts: env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',') : [],
+    allowedHosts: ["cinecritic-load-balancer-351804049.ap-southeast-2.elb.amazonaws.com"],
+    port: 5173,
+    watch: {
+      usePolling: true,
     },
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './tests/setupTests.js',
-    },
-  }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setupTests.js',
+  },
 });
+
