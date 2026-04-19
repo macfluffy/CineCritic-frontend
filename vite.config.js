736a09813ecb6766f3edc03ca81eb8f94b62f694
macfluffy <shadowimitation@hotmail.com>
2026-04-19 19:05:00 +1000

    feat: Fixed a syntax error in defineconfig. Now properly hides the allowedhosts in env file

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,21 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  
+
   return {
     plugins: [react()],
     server: {
       host: true,
       allowedHosts: env.VITE_ALLOWED_HOSTS ? env.VITE_ALLOWED_HOSTS.split(',') : [],
       port: 5173,
-    watch: {
-      usePolling: true,
+      watch: {
+        usePolling: true,
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './tests/setupTests.js',
-  },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './tests/setupTests.js',
+    },
+  }
 });
